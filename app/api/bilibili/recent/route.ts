b9fa7157577fb2f075aeb695c21cf3a420fd664f
Chongxi <141206905+ChongxiSama@users.noreply.github.com>
2026-04-28 18:44:13 +0800

    Update route.ts

--- app/api/bilibili/recent/route.ts
+++ app/api/bilibili/recent/route.ts
@@ -4,18 +4,18 @@ export const revalidate = 60
 export async function GET() {
   try {
     const res = await fetch(
-      'https://api.bilibili.com/x/web-interface/history/cursor?ps=1',
-      {
-        headers: {
-  Cookie: `SESSDATA=${process.env.BILIBILI_SESSDATA}; bili_jct=${process.env.BILIBILI_JCT}`,
-  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-  Referer: 'https://www.bilibili.com',
-  Origin: 'https://www.bilibili.com',
-  Accept: 'application/json, text/plain, */*',
-  'Accept-Language': 'zh-CN,zh;q=0.9',
-      }
-    )
-
+  'https://api.bilibili.com/x/web-interface/history/cursor?ps=1',
+  {
+    headers: {
+      Cookie: `SESSDATA=${process.env.BILIBILI_SESSDATA}; bili_jct=${process.env.BILIBILI_JCT}`,
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+      Referer: 'https://www.bilibili.com',
+      Origin: 'https://www.bilibili.com',
+      Accept: 'application/json, text/plain, */*',
+      'Accept-Language': 'zh-CN,zh;q=0.9',
+    },
+  }
+)
     const data = await res.json()
     
     if (data.code !== 0) {
