87a58bfb4d66fa3a81fdf6e94fa2d72159d03c60
ChongxiSama <chongxi3555@proton.me>
2026-06-02 08:20:33 +0000

    fix: remove three-color stripe from footer

--- app/tool/jieyuan/page.tsx
+++ app/tool/jieyuan/page.tsx
@@ -86,11 +86,6 @@ export default function JieYuanPage() {
             </div>
           </div>
 
-          <div className="absolute bottom-0 left-0 right-0 h-[3px] flex z-10">
-            <div className="bg-rl-teal w-1/3"></div>
-            <div className="bg-rl-gold w-1/3"></div>
-            <div className="bg-rl-red w-1/3"></div>
-          </div>
         </div>
       </div>
     </div>
